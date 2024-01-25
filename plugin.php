4f2f1d933b1f5969c470782785960636079edc7a
jung <tan@gmx.sg>
2024-01-25 22:06:52 +0800

    Update plugin.php
    add checking function

--- plugin.php
+++ plugin.php
@@ -10,3 +10,9 @@ Author URI: https://jung.bz/
 
 // Hook custom function into the 'shunt_add_new_link' filter
 yourls_add_filter( 'shunt_add_new_link', 'limit_keyword_length' );
+
+// Check custom keyword length and return an error if it exceeds the max or min length limit
+function limit_keyword_length( $error, $url, $keyword ) {
+	$max_length = 100; // Set max length limit for custom keyword
+	$min_length = 1; // Set min length limit for custom keyword
+	$length = strlen( $keyword );
