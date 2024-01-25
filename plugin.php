e185c52a9abb6d055fa64d30ab9a34775c5f35b6
jung <tan@gmx.sg>
2024-01-25 22:03:40 +0800

    Create plugin.php
    create plugin header

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Limit Custom Keyword Length
+Plugin URI: https://github.com/suryatanjung/yourls-limit-custom-keyword-length/
+Description: This plugin limits the min and max number of characters for custom keyword
+Version: 1.0
+Author: Surya Tanjung
+Author URI: https://jung.bz/
+*/
